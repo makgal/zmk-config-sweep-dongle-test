eee82cd21eac4b20791fae3bd1e07716b4f0a1f3
makgal <47987236+makgal@users.noreply.github.com>
2025-10-19 21:21:32 +0300

    Update custom_status_screen.h

--- boards/shields/cradio/custom_status_screen.h
+++ boards/shields/cradio/custom_status_screen.h
@@ -1,5 +1,14 @@
 #pragma once
 
 #include <lvgl.h>
+#include <zephyr/kernel.h>
 
 lv_obj_t *zmk_display_status_screen();
+
+struct zmk_widget_dongle_battery_status {
+    sys_snode_t node;
+    lv_obj_t *obj;
+};
+
+int zmk_widget_dongle_battery_status_init(struct zmk_widget_dongle_battery_status *widget, lv_obj_t *parent);
+lv_obj_t *zmk_widget_dongle_battery_status_obj(struct zmk_widget_dongle_battery_status *widget);
