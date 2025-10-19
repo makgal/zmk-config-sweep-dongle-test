1b7ea9e5efa7a0f2cab91cfe41acda6930cb4457
makgal <47987236+makgal@users.noreply.github.com>
2025-10-19 20:10:40 +0300

    Update custom_status_screen.c

--- boards/shields/cradio/custom_status_screen.c
+++ boards/shields/cradio/custom_status_screen.c
@@ -1,31 +1,21 @@
-#include "custom_status_screen.h"
 #include <lvgl.h>
-
-static lv_style_t global_style;
-static lv_color_t canvas_buffer[20 * 20]; // буфер для 20x20 пикселей
+#include "custom_status_screen.h"
 
 lv_obj_t *zmk_display_status_screen(void) {
+    // Создаём главный экран
     lv_obj_t *screen = lv_obj_create(NULL);
 
-    lv_style_init(&global_style);
-    lv_style_set_text_font(&global_style, &lv_font_unscii_8);
-    lv_style_set_text_letter_space(&global_style, 1);
-    lv_style_set_text_line_space(&global_style, 1);
-    lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
+    // Инициализируем стиль для квадрата
+    static lv_style_t square_style;
+    lv_style_init(&square_style);
+    lv_style_set_bg_color(&square_style, lv_color_white());
+    lv_style_set_bg_opa(&square_style, LV_OPA_COVER);
 
-    // Canvas вместо обычного объекта
-    lv_obj_t *canvas = lv_canvas_create(screen);
-    lv_canvas_set_buffer(canvas, canvas_buffer, 20, 20, LV_IMG_CF_TRUE_COLOR);
-    lv_obj_align(canvas, LV_ALIGN_CENTER, 0, 0);
-
-    lv_canvas_fill_bg(canvas, lv_color_black(), LV_OPA_COVER);
-
-    // Рисуем белый квадрат
-    lv_draw_rect_dsc_t rect_dsc;
-    lv_draw_rect_dsc_init(&rect_dsc);
-    rect_dsc.bg_color = lv_color_white();
-    rect_dsc.bg_opa = LV_OPA_COVER;
-    lv_canvas_draw_rect(canvas, 0, 0, 10, 10, &rect_dsc);
+    // Создаём квадрат на экране
+    lv_obj_t *square = lv_obj_create(screen);
+    lv_obj_set_size(square, 20, 20);           // размеры квадрата 20x20 px
+    lv_obj_add_style(square, &square_style, 0);
+    lv_obj_align(square, LV_ALIGN_CENTER, 0, 0); // по центру экрана
 
     return screen;
 }
